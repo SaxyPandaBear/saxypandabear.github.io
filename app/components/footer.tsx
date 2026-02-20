94f15e72550cecd4ab356744db11e443d7284419
Andrew Huynh <17056456+SaxyPandaBear@users.noreply.github.com>
2026-02-20 17:09:23 -0500

    fixed external links, filled out sections

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -17,14 +17,14 @@ function ArrowIcon() {
 
 export default function Footer() {
   return (
-    <footer className="mb-16">
-      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300 dark:bg-black">
+    <footer className="mb-16 px-8">
+      <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
+            href="https://github.com/SaxyPandaBear"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
@@ -35,7 +35,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://www.linkedin.com/in/andrewhuynh4007/"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">linkedin</p>
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="https://github.com/SaxyPandaBear/saxypandabear.github.io"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">view source</p>
--- app/page.tsx
+++ app/page.tsx
@@ -1,121 +1,109 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        {/* <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        /> */}
-        {/* <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            About Me
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-          <div />
-        </div> */}
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold tracking-tight text-black dark:text-zinc-50">
             About Me
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Something goes here about me.
+          <p className="max-w-xl text-lg text-zinc-600 dark:text-zinc-400">
+            I'm a career software engineer with roots in the Pacific Northwest and the mid-Atlantic.
+            My biggest motivators are cultivating a strong local community, whether in the fine arts,
+            or access to affordable housing and food. A fan of live music, world travel, and cooking among
+            many things, there are lots of facets to life outside of work. 
           </p>
           <div />
         </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold tracking-tight text-black dark:text-zinc-50">
             Work Experience
           </h1>
           <div>
-            <p className="max-w-md text-lg text-zinc-600 dark:text-zinc-400">
-            <span className="text-zinc-950 dark:text-zinc-50">Amazon Web Services</span>{" "}<span className="italic text-sm">2020 - 2025</span>
+            <p className="max-w-lg text-xl text-zinc-600 dark:text-zinc-400">
+              <span className="text-zinc-950 dark:text-zinc-50 underline font-semibold">Amazon Web Services</span>
+              {" "}
+              <span className="text-sm">Senior Software Engineer</span>
+              {" "}
+              <span className="italic text-xs">2020-2025</span>
             </p>
             <ul className="px-8 list-disc italic text-sm text-zinc-950 dark:text-zinc-50">
-              <li>Contributor, maintainer, and primary code reviewer of open-source AWS CloudWatch agent project, responsible for global releases targetting Windows, Linux and MacOS, and as a standalone project or Docker image.</li>
+              <li>
+                Contributor, maintainer, and primary code reviewer of open-source AWS CloudWatch agent project, 
+                responsible for global releases targetting Windows, Linux and MacOS, and as a standalone project or Docker image
+              </li>
+              <li>Designed, implemented, benchmarked and released{" "}
+                <a
+                  href="https://aws.amazon.com/blogs/mt/optimize-log-collection-with-amazon-cloudwatch-agent-log-filter-expressions/"
+                  className="text-zinc-950 font-semibold underline dark:text-zinc-50"
+                >
+                  feature
+                </a>{" "}
+                for pattern matching for log filtering in AWS CloudWatch agent to help optimize AWS costs for customers
+              </li>
               <li>Triaged external customer escalations with hands-on troubleshooting of AWS CloudWatch agent issues</li>
               <li>Prototyped GDPR-compliant redaction for telemetry metadata exfiltrated from airgrapped environments</li>
               <li>Designed, implemented, and released internal telemetry agent plugin to passively monitor JVM processes for resource optimization analyses</li>
-              <li>Implemented high-volume, daily ETL workflows in AWS EMR, orchestrated via AWS Step Functions</li>
+              <li>Implemented high-volume, daily ETL workflows in AWS EMR, orchestrated via AWS Step Functions and written as infrastructure-as-code in AWS CDK</li>
             </ul>
           </div>
           <div>
-            <p className="max-w-md text-lg text-zinc-600 dark:text-zinc-400">
-            <span className="text-zinc-950 dark:text-zinc-50">Capital One</span>{" "}<span className="italic text-sm">2018 - 2020</span>
+            <p className="max-w-lg text-xl text-zinc-600 dark:text-zinc-400">
+              <span className="text-zinc-950 dark:text-zinc-50 underline font-semibold">Capital One</span>
+              {" "}
+              <span className="text-sm">Senior Software Engineer</span>
+              {" "}
+              <span className="italic text-xs">2018-2020</span>
             </p>
             <ul className="px-8 list-disc italic text-sm text-zinc-950 dark:text-zinc-50">
-              <li>abc</li>
-              <li>abc</li>
-              <li>abc</li>
-            </ul>
-          </div>
-          <div>
-            <p className="max-w-md text-lg text-zinc-600 dark:text-zinc-400">
-            <span className="text-zinc-950 dark:text-zinc-50">Capital One</span>{" "}<span className="italic text-sm">2018 - 2018</span>
-            </p>
-            <ul className="px-8 list-disc italic text-sm text-zinc-950 dark:text-zinc-50">
-              <li>abc</li>
-              <li>abc</li>
-              <li>abc</li>
+              <li>Implemented SSL termination for gRPC and HTTP API endpoints for enterprise data streaming service</li>
+              <li>Designed and implemented cross-region, eventually-consistent PII scanning for sensitive data at rest</li>
+              <li>Core code reviewer and maintainer for project with over 70 unique contributors</li>
+              <li>Designed, implemented, benchmarked, and released data redaction mechanism for data in-transit</li>
             </ul>
           </div>
           <div />
         </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold tracking-tight text-black dark:text-zinc-50">
             Personal Projects
           </h1>
-          <p className="max-w-md text-lg text-zinc-600 dark:text-zinc-400">
-            <span className="text-zinc-950 dark:text-zinc-50">TwitchSongRequests</span>
-          </p>
-          <p className="max-w-md text-md italic leading-4 text-zinc-600 dark:text-zinc-400">
-            Backend service that connects Twitch webhook events to update and queue up songs in a user's
-            Spotify player. Written in Go.
-          </p>
+          <div>
+            <p className="max-w-md text-xl text-zinc-600 dark:text-zinc-400">
+              <span className="text-zinc-950 dark:text-zinc-50">TwitchSongRequests</span>{" - "}<span className="text-md font-semibold">Written in Go</span>
+            </p>
+            <p className="max-w-xl text-md italic text-zinc-600 dark:text-zinc-400">
+              Backend service that connects Twitch webhook events to update and queue up songs in a user's
+              Spotify player. Deployed and servicing several partnered Twitch streamers
+            </p>
+          </div>
+          <div>
+            <p className="max-w-md text-xl text-zinc-600 dark:text-zinc-400">
+              <span className="text-zinc-950 dark:text-zinc-50">Spotify Recapped</span>{" - "}<span className="text-md font-semibold">Written in Python</span>
+            </p>
+            <p className="max-w-xl text-md italic text-zinc-600 dark:text-zinc-400">
+              Script that reads the listening history data for a user from Spotify and outputs statistics, top songs and artists, 
+              in an attempt to reverse-engineer the implementation for the annual Spotify Wrapped
+            </p>
+          </div>
           <div />
         </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold tracking-tight text-black dark:text-zinc-50">
             Hobbies
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+          <ul className="px-8 list-disc max-w-2xl text-lg text-zinc-950 dark:text-zinc-50">
+            <li>
+              Piano and saxophone player in{" "}
+              <a
+                href="https://thebroadstrokesband.com"
+                className="italic font-semibold text-zinc-600 dark:text-zinc-400"
+              >
+                The Broad Strokes
+              </a>{" "}, and plugged in to the Richmond jazz and rnb music scene
+            </li>
+            <li>Avid homecook in multiple local supper clubs, and volunteer cook for free food distribution</li>
+            {/* TODO: add gallery photos? */}
+          </ul>
         </div>
       </main>
     </div>
