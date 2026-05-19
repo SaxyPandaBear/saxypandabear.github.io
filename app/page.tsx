94640d3199ba530453f384ebdae04cfcff4fde14
Andrew Huynh <17056456+SaxyPandaBear@users.noreply.github.com>
2026-05-19 11:23:51 -0400

    delete hsm stuff cause its not needed now

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,10 @@ export default function Home() {
             About Me
           </h1>
           <p className="max-w-xl text-md text-zinc-600 dark:text-zinc-400">
-            I'm a career software engineer with roots in the Pacific Northwest and the mid-Atlantic.
-            My biggest motivators are cultivating a strong local community, whether in the fine arts,
-            or access to affordable housing and food. A fan of live music, world travel, and food among
+            I'm a career software engineer with roots in the Pacific Northwest
+            and the mid-Atlantic. My biggest motivators are cultivating a strong
+            local community, whether in the fine arts, or access to affordable
+            housing and food. A fan of live music, world travel, and food among
             other things.
           </p>
           <div />
@@ -22,45 +23,76 @@ export default function Home() {
           </h1>
           <div>
             <p className="max-w-lg text-xl text-zinc-600 dark:text-zinc-400">
-              <span className="text-zinc-950 dark:text-zinc-50 underline font-semibold">Amazon Web Services</span>
-              {" "}
-              <span className="text-sm">Senior Software Engineer</span>
-              {" "}
+              <span className="text-zinc-950 dark:text-zinc-50 underline font-semibold">
+                Amazon Web Services
+              </span>{" "}
+              <span className="text-sm">Senior Software Engineer</span>{" "}
               <span className="italic text-xs">2020-2025</span>
             </p>
             <ul className="px-8 list-disc italic text-sm text-zinc-950 dark:text-zinc-50">
               <li>
-                Contributor, maintainer, and primary code reviewer of open-source AWS CloudWatch agent project, 
-                responsible for global releases targetting Windows, Linux and MacOS, and as a standalone project or Docker image
+                Contributor, maintainer, and primary code reviewer of
+                open-source AWS CloudWatch agent project, responsible for global
+                releases targetting Windows, Linux and MacOS, and as a
+                standalone project or Docker image
               </li>
-              <li>Designed, implemented, benchmarked and released feature and accompanying{" "}
+              <li>
+                Designed, implemented, benchmarked and released feature and
+                accompanying{" "}
                 <a
                   href="https://aws.amazon.com/blogs/mt/optimize-log-collection-with-amazon-cloudwatch-agent-log-filter-expressions/"
                   className="text-zinc-950 font-semibold underline dark:text-zinc-50"
                 >
                   blog post
                 </a>{" "}
-                for pattern matching for log filtering in AWS CloudWatch agent to help optimize AWS costs for customers
+                for pattern matching for log filtering in AWS CloudWatch agent
+                to help optimize AWS costs for customers
               </li>
-              <li>Triaged external customer escalations with hands-on troubleshooting of AWS CloudWatch agent issues</li>
-              <li>Prototyped GDPR-compliant redaction for telemetry metadata exfiltrated from airgrapped environments</li>
-              <li>Designed, implemented, and released internal telemetry agent plugin to passively monitor JVM processes for resource optimization analyses</li>
-              <li>Implemented high-volume, daily ETL workflows in AWS EMR, orchestrated via AWS Step Functions and written as infrastructure-as-code in AWS CDK</li>
+              <li>
+                Triaged external customer escalations with hands-on
+                troubleshooting of AWS CloudWatch agent issues
+              </li>
+              <li>
+                Prototyped GDPR-compliant redaction for telemetry metadata
+                exfiltrated from airgrapped environments
+              </li>
+              <li>
+                Designed, implemented, and released internal telemetry agent
+                plugin to passively monitor JVM processes for resource
+                optimization analyses
+              </li>
+              <li>
+                Implemented high-volume, daily ETL workflows in AWS EMR,
+                orchestrated via AWS Step Functions and written as
+                infrastructure-as-code in AWS CDK
+              </li>
             </ul>
           </div>
           <div>
             <p className="max-w-lg text-xl text-zinc-600 dark:text-zinc-400">
-              <span className="text-zinc-950 dark:text-zinc-50 underline font-semibold">Capital One</span>
-              {" "}
-              <span className="text-sm">Senior Software Engineer</span>
-              {" "}
+              <span className="text-zinc-950 dark:text-zinc-50 underline font-semibold">
+                Capital One
+              </span>{" "}
+              <span className="text-sm">Senior Software Engineer</span>{" "}
               <span className="italic text-xs">2018-2020</span>
             </p>
             <ul className="px-8 list-disc italic text-sm text-zinc-950 dark:text-zinc-50">
-              <li>Implemented SSL termination for gRPC and HTTP API endpoints for enterprise data streaming service</li>
-              <li>Designed and implemented cross-region, eventually-consistent PII scanning for sensitive data at rest</li>
-              <li>Core code reviewer and maintainer for project with over 70 unique contributors</li>
-              <li>Designed, implemented, benchmarked, and released data redaction mechanism for data in-transit</li>
+              <li>
+                Implemented SSL termination for gRPC and HTTP API endpoints for
+                enterprise data streaming service
+              </li>
+              <li>
+                Designed and implemented cross-region, eventually-consistent PII
+                scanning for sensitive data at rest
+              </li>
+              <li>
+                Core code reviewer and maintainer for project with over 70
+                unique contributors
+              </li>
+              <li>
+                Designed, implemented, benchmarked, and released data redaction
+                mechanism for data in-transit
+              </li>
             </ul>
           </div>
           <div />
@@ -71,20 +103,31 @@ export default function Home() {
           </h1>
           <div>
             <p className="max-w-md text-xl text-zinc-600 dark:text-zinc-400">
-              <span className="text-zinc-950 dark:text-zinc-50">TwitchSongRequests</span>{" - "}<span className="text-sm font-semibold">Written in Go</span>
+              <span className="text-zinc-950 dark:text-zinc-50">
+                TwitchSongRequests
+              </span>
+              {" - "}
+              <span className="text-sm font-semibold">Written in Go</span>
             </p>
             <p className="max-w-xl text-md italic text-zinc-600 dark:text-zinc-400">
-              Backend service that connects Twitch webhook events to update and queue up songs in a user's
-              Spotify player. Deployed and servicing several partnered Twitch streamers
+              Backend service that connects Twitch webhook events to update and
+              queue up songs in a user's Spotify player. Deployed and servicing
+              several partnered Twitch streamers
             </p>
           </div>
           <div>
             <p className="max-w-md text-xl text-zinc-600 dark:text-zinc-400">
-              <span className="text-zinc-950 dark:text-zinc-50">Spotify Recapped</span>{" - "}<span className="text-sm font-semibold">Written in Python</span>
+              <span className="text-zinc-950 dark:text-zinc-50">
+                Spotify Recapped
+              </span>
+              {" - "}
+              <span className="text-sm font-semibold">Written in Python</span>
             </p>
             <p className="max-w-xl text-md italic text-zinc-600 dark:text-zinc-400">
-              Script that reads the listening history data for a user from Spotify and outputs statistics, top songs and artists, 
-              in an attempt to reverse-engineer the implementation for the annual Spotify Wrapped
+              Script that reads the listening history data for a user from
+              Spotify and outputs statistics, top songs and artists, in an
+              attempt to reverse-engineer the implementation for the annual
+              Spotify Wrapped
             </p>
           </div>
           <div />
@@ -102,30 +145,16 @@ export default function Home() {
                   className="italic font-semibold text-zinc-600 dark:text-zinc-400"
                 >
                   The Broad Strokes
-                </a>{" "}, and plugged in to the Richmond jazz and rnb music scene
+                </a>{" "}
+                , and plugged in to the Richmond jazz and rnb music scene
               </li>
-              <li>Avid homecook in multiple local supper clubs, and volunteer cook for free food distribution</li>
+              <li>
+                Avid homecook in multiple local supper clubs, and volunteer cook
+                for free food distribution
+              </li>
             </ul>
           </div>
           <Gallery />
-          <div />
-        </div>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold tracking-tight uppercase text-black dark:text-zinc-50">
-            High School Musical, The Birthday: The Musical
-          </h1>
-          <p className="max-w-xl text-md text-zinc-600 dark:text-zinc-400">
-            Access the screenplay {" "}
-            <a
-              href="https://github.com/SaxyPandaBear/saxypandabear.github.io/blob/master/public/hsm/HSM_screenplay.pdf"
-              rel="noopener noreferrer"
-              target="_blank"
-              className="italic font-semibold text-zinc-600 dark:text-zinc-400"
-            >
-            here 
-            </a> 
-            .
-          </p>
           <div />
         </div>
       </main>
