083dadfc8197632c65b48a83bc40e2eec9bf31c4
Andrew Huynh <17056456+SaxyPandaBear@users.noreply.github.com>
2026-08-26 15:45:34 -0400

    wtf

--- app/lib/transcriptions.ts
+++ app/lib/transcriptions.ts
@@ -24,7 +24,7 @@ export class Song {
 
   public static fromPath(s: string): Song {
     // assume in the form /transcriptions/[name] artist/part/filename.pdf
-    const result = SONG_PATTERN.exec(decodeURIComponent(decodeURIComponent(s)))
+    const result = SONG_PATTERN.exec(decodeURIComponent(s).replace("%2F", "/"))
     if (result == null) {
       throw Error("Unable to parse file path");
     }
